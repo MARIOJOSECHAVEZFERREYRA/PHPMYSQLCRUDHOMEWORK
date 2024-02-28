b97c6a77e1f8e8826fb2de81dc618539821cd708
MARIOJOSECHAVEZFERREYRA <mariojosechavezferreyra@gmail.com>
2024-02-28 13:09:54 +0300

    style bugs solved

--- db.php
+++ db.php
@@ -5,7 +5,7 @@ session_start();
 $conn = mysqli_connect(
   'localhost',
   'root',
-  'password123',
+  '',
   'php_mysql_crud'
 ) or die(mysqli_error($mysqli));
 
--- index.php
+++ index.php
@@ -1,70 +1,71 @@
-<?php include("db.php")?>
+<?php include("db.php"); ?>
 
-<?php include("includes/header.php")?>
+<?php include('includes/header.php'); ?>
 
-    <div class="container p-4">
+<main class="container p-4">
+  <div class="row">
+    <div class="col-md-4">
+      <!-- MESSAGES -->
 
-        <div class="row">
+      <?php if (isset($_SESSION['message'])) { ?>
+      <div class="alert alert-<?= $_SESSION['message_type']?> alert-dismissible fade show" role="alert">
+        <?= $_SESSION['message']?>
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <?php session_unset(); } ?>
 
-            <div class="col-md-4">
-
-            <?php if(isset($_SESSION['message'])){ ?>
-                <div class="alert alert-<?php $_SESSION['message-type']; ?> alert-dismissible fade show" role="alert">
-                <?php $_SESSION['message'] ?>
-                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-              </div>
-            <?php session_unset(); } ?>
-
-                <div class="card card-body">
-                    <form action="save_task.php" method="POST">
-                        <div class="form-group">
-                        <input type="text" name="title" class="form-control" placeholder="Task Title" autofocus>
-                        </div>
-                        <div class="form-group">
-                        <textarea name="description" id="" rows="2" class="form-control" placeholder="Task Description"></textarea>
-                        </div>
-                        <input type="submit" class="btn btn-success btn-block" name="save_task" value="Save Task">
-                    </form>
-                </div>
-            </div>
-            <div class="col-md-8">
-                <table class="table table-bordered ">
-                    <thead>
-                        <tr>
-                            <th>Title</th>
-                            <th>Description</th>
-                            <th>Created At</th>
-                            <th>Actions</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php
-                            $query = "SELECT * FROM task";
-                            $result_tasks = mysqli_query($conn, $query);
-
-                            while($row = mysqli_fetch_array($result_tasks)){ ?>
-                                <tr>
-                                    <td><?php echo $row['title']?></td>
-                                    <td><?php echo $row['description']?></td>
-                                    <td><?php echo $row['created_at']?></td>
-                                    <th>
-                                        <a href="edit.php?id=<?php echo $row['id']?>" class="btn btn-secondary">
-                                        <i class="fa-regular fa-pen-to-square"></i>
-                                        </a>
-                                    </th>
-                                    <th>
-                                        <a href="delete.php?id=<?php echo $row['id']?>" class="btn btn-danger">
-                                        <i class="fa-solid fa-eraser"></i>
-                                        </a>
-                                    </th>
-                                </tr>
-                            <?php } ?> 
-
-                        ?>
-                    </tbody>
-                </table>
-            </div>
-        </div>
+      <!-- ADD TASK FORM -->
+      <div class="card card-body">
+        <form action="save_task.php" method="POST">
+          <div class="form-group mb-3">
+            <input type="text" name="title" class="form-control" placeholder="Task Title" autofocus>
+          </div>
+          <div class="form-group mb-3">
+            <textarea name="description" rows="2" class="form-control" placeholder="Task Description"></textarea>
+          </div>
+          <div class="form-group">
+          <input type="submit" name="save_task" class="btn btn-success" value="Save Task" style="width: 100%;">
+          </div>
+        </form>
+      </div>
     </div>
+    <div class="col-md-8">
+      <table class="table table-bordered">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Description</th>
+            <th>Created At</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
 
-<?php include("includes/footer.php")?>
+          <?php
+          $query = "SELECT * FROM task";
+          $result_tasks = mysqli_query($conn, $query);    
+
+          while($row = mysqli_fetch_assoc($result_tasks)) { ?>
+          <tr>
+            <td><?php echo $row['title']; ?></td>
+            <td><?php echo $row['description']; ?></td>
+            <td><?php echo $row['created_at']; ?></td>
+            <td>
+              <a href="edit_task.php?id=<?php echo $row['id']?>" class="btn btn-secondary">
+                <i class="fas fa-marker"></i>
+              </a>
+              <a href="delete_task.php?id=<?php echo $row['id']?>" class="btn btn-danger">
+                <i class="far fa-trash-alt"></i>
+              </a>
+            </td>
+          </tr>
+          <?php } ?>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</main>
+
+<?php include('includes/footer.php'); ?>
